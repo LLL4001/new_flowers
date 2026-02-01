3464fe7ff682feb9245ef5a55dde0861904ba2dc
LLL4001 <129660644+LLL4001@users.noreply.github.com>
2026-02-01 12:29:42 +0100

    paris hitbox fix

--- init.lua
+++ init.lua
@@ -30,7 +30,7 @@ core.register_node("new_flowers:paris_quadrifolia_young", {
 	groups = { snappy = 3, dig_immediate = 1, flammable = 2, eatable = 2 },
 	sounds = default.node_sound_leaves_defaults(),
 	selection_box = {
-		type = "fixed", fixed = { -0.15, -0.5, -0.15, 0.15, 0.2, 0.15 },
+		type = "fixed", fixed = { -0.10, -0.5, -0.15, 0.15, -0.15, 0.15 },
 	},
 	visual_scale = 0.8,
 })
@@ -50,7 +50,7 @@ core.register_node("new_flowers:paris_quadrifolia", {
 	groups = { snappy = 3, dig_immediate = 1, flammable = 2, eatable = 2 },
 	sounds = default.node_sound_leaves_defaults(),
 	selection_box = {
-		type = "fixed", fixed = { -0.15, -0.5, -0.15, 0.15, 0.2, 0.15 },
+		type = "fixed", fixed = { -0.15, -0.5, -0.20, 0.20, 0.15, 0.20 },
 	},
 	visual_scale = 0.8,
 })
@@ -70,7 +70,7 @@ core.register_node("new_flowers:paris_quadrifolia_berry", {
 	groups = { snappy = 3, dig_immediate = 1, flammable = 2, eatable = 2 },
 	sounds = default.node_sound_leaves_defaults(),
 	selection_box = {
-		type = "fixed", fixed = { -0.15, -0.5, -0.15, 0.15, 0.2, 0.15 },
+		type = "fixed", fixed = { -0.15, -0.5, -0.20, 0.20, 0.25, 0.20 },
 	},
 	visual_scale = 0.8,
 })
