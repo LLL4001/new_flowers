fd502c8e6cc1023f299c2ec20f810105c4752015
LLL4001 <129660644+LLL4001@users.noreply.github.com>
2026-02-01 12:43:02 +0100

    hitbox plantain & daisy fix

--- init.lua
+++ init.lua
@@ -94,7 +94,7 @@ core.register_node("new_flowers:plantain", {
 	groups = { snappy = 3, dig_immediate = 1, flammable = 2, eatable = 2 },
 	sounds = default.node_sound_leaves_defaults(),
 	selection_box = {
-		type = "fixed", fixed = { -0.15, -0.5, -0.15, 0.15, 0.2, 0.15 },
+		type = "fixed", fixed = { -0.18, -0.5, -0.18, 0.18, -0.15, 0.18 },
 	},
 	visual_scale = 0.8,
 })
@@ -120,7 +120,7 @@ core.register_node("new_flowers:daisies", {
 	groups = { snappy = 3, dig_immediate = 1, flammable = 2, eatable = 2 },
 	sounds = default.node_sound_leaves_defaults(),
 	selection_box = {
-		type = "fixed", fixed = { -0.15, -0.5, -0.15, 0.15, 0.2, 0.15 },
+		type = "fixed", fixed = { -0.25, -0.5, -0.25, 0.25, -0.2, 0.25 },
 	},
 	visual_scale = 0.8,
 })
